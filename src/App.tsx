dc309bd797479b231ff98fbcf67071c6f0e2f16f
Fabio Pontes <faabiopontes@gmail.com>
2020-09-04 14:54:15 -0300

    feat: Global Styles with Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import GlobalStyles from './styles/global';
 
 const App: React.FC = () => (
-  <div className="App">
-    <p>Basic Functional Component</p>
-  </div>
+  <>
+    <div className="App">
+      <p>Basic Functional Component</p>
+    </div>
+    <GlobalStyles />
+  </>
 );
 
 export default App;
